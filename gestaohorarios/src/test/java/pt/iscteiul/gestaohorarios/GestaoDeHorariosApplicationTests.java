93c24cdb7f320735a1c23e29bb6a69fbef10961c
Gabriel Cardoso <114010143+gabriel-bitrefill@users.noreply.github.com>
2023-04-07 12:14:55 +0100

    Criação/Config do backend Java Spring

---
+++ gestaohorarios/src/test/java/pt/iscteiul/gestaohorarios/GestaoDeHorariosApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.iscteiul.gestaohorarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDeHorariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
