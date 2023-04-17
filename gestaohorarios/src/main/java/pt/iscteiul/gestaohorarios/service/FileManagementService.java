e246add9eaf749e24a334c40d8920fe2bfd08f63
Gabie02 <gpjle@iscte-iul.pt>
2023-04-17 18:38:45 +0100

    Introdução de um logger

--- gestaohorarios/src/main/java/pt/iscteiul/gestaohorarios/service/FileManagementService.java
+++ gestaohorarios/src/main/java/pt/iscteiul/gestaohorarios/service/FileManagementService.java
@@ -1,6 +1,9 @@
 package pt.iscteiul.gestaohorarios.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.logging.LoggingSystem;
 import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,21 +16,22 @@ import java.nio.file.Path;
 import java.util.List;
 import java.util.Objects;
 
+
 @Service
 public class FileManagementService {
     @Autowired
     private ConversorCSVJSON conversorCSVJSON;
     public static final Path CSV_UPLOAD_PATH = Path.of(System.getProperty("user.dir") + "/data/horarios/csv/");
     public static final Path JSON_UPLOAD_PATH = Path.of(System.getProperty("user.dir") + "/data/horarios/json/");
+    Logger logger = LoggerFactory.getLogger(FileManagementService.class);
 
     public boolean uploadFile(MultipartFile file) {
         Path destinationPath = JSON_UPLOAD_PATH;
         String originalFileName = file.getOriginalFilename();
         try {
-            if (Objects.requireNonNull(originalFileName).contains("csv")){
+            if (Objects.requireNonNull(originalFileName).contains("csv")) {
                 destinationPath = CSV_UPLOAD_PATH;
 
-                //TODO Adicinar algum tipo de ID ao ficheiro para não haver conflitos
                 File destination = new File(destinationPath + "\\" + file.getOriginalFilename());
                 file.transferTo(destination);
 
@@ -47,8 +51,7 @@ public class FileManagementService {
 
 
         } catch (IOException e) {
-            System.err.println("Erro ao guardar ficheiro");
-            e.printStackTrace();
+            logger.error("Erro ao guardar o ficheiro", e);
             return false;
         }
         return true;
@@ -61,12 +64,12 @@ public class FileManagementService {
 
     public UrlResource getFile(String name) {
         Path searchingPath = JSON_UPLOAD_PATH;
-        try {
-            if (name.contains("csv"))
-                searchingPath = CSV_UPLOAD_PATH;
+        if (name.contains("csv"))
+            searchingPath = CSV_UPLOAD_PATH;
+        try (var wantedDir = Files.list(searchingPath)) {
 
-            var wantedFile = Files.list(searchingPath)
-                    .filter(f -> f.getFileName().toString().equals(name))
+
+            var wantedFile = wantedDir.filter(f -> f.getFileName().toString().equals(name))
                     .toList();
 
             if (!wantedFile.isEmpty())
@@ -74,7 +77,7 @@ public class FileManagementService {
 
 
         } catch (IOException e) {
-            System.err.println("Erro ao buscar o ficheiro");
+            logger.error("Erro ao buscar o ficheiro", e);
         }
 
         return null;
