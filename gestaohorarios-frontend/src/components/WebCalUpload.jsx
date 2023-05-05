a439c4c3ad5ef43d623a94bee0f08b84cd54758a
tegco <tegco@iscte-iul.pt>
2023-05-05 15:44:55 +0100

    Config. proxy inicial e do fetch de dados

--- gestaohorarios-frontend/src/components/WebCalUpload.jsx
+++ gestaohorarios-frontend/src/components/WebCalUpload.jsx
@@ -6,14 +6,56 @@ import Typography from '@mui/material/Typography';
 
 const WebCalUpload = () => {
   const [uri, setUri] = useState('');
+  console.log('Uri:', uri)
+  //const [events, setEvents] = useState([]);  
  
-  const handleWebcalChange= async (event) => {
+  const handleChange = async (event) => {
     setUri(event.target.value);
-  };
+    console.log('On Change:', uri)
+    //processCalendar(file);
+  }
 
-  const handleWebCalUpload = async (event) => {
+  const fetchCalendar = async (uri) => {
+
+    const httpsUrl = uri.replace('webcal', 'https');
+    console.log('httpsUrl:', httpsUrl);
+
+    const response = await fetch('/icalendar', { timeout: 10000 }, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ targetUrl: httpsUrl }), 
+        
+      });
+      
+      if (!response.ok) {
+        throw new Error(`Error fetching the file: ${response.statusText}`);
+      }
+      
+      const data = await response.text();
+      console.log('DADOS DO CALENDARIO:', data);
+      return new File([data], { type: 'text/calendar' });
+    };
+
+  /* const getFilenameFromUrl = (url) => {
+    const parts = url.split('/');
+    const filename = parts[parts.length - 1];
+
+    if (filename.endsWith('.ics')) {
+    return filename;
+    } else {
+    console.error('Invalid URL: File should be an ICS file');
+    return null;
+  }
+} */
+
+  const handleUpload = async (event) => {
     event.preventDefault();
-}
+    const file = await fetchCalendar(uri);
+    console.log('Ficheiro:', file.name)
+    //processCalendar(file);
+    }
 
     return (
         <Box>
@@ -33,13 +75,13 @@ const WebCalUpload = () => {
                 size="small"
                 fullWidth
                 value={uri}
-                onChange={handleWebcalChange}
+                onChange={handleChange}
                 style={{ marginBottom: '16px' }}
             />
             </Box>
 
             <Box display="flex" alignItems="center" justifyContent="center" style={{ width: '100%' }}>
-                <Button variant="contained" color="primary" onClick={handleWebCalUpload}>
+                <Button variant="contained" color="primary" onClick={handleUpload}>
                     Send
                 </Button>
             </Box>
--- gestaohorarios-frontend/src/setupProxy.js
+++ gestaohorarios-frontend/src/setupProxy.js
@@ -1,4 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const http = require('http');
+const httpProxy = require('http-proxy');
+const bodyParser = require('body-parser');
 
 module.exports = function (app) {
   app.use(
@@ -7,5 +10,19 @@ module.exports = function (app) {
       target: 'http://localhost:8080',
       changeOrigin: true,
     })
-  );
+  )
+
+  app.use('/fetch-url', bodyParser.json(), (req, res) => {
+    const { targetUrl } = req.body;
+
+    app.use(
+      '/icalendar',
+      createProxyMiddleware({
+        target: targetUrl,
+        changeOrigin: true,
+      })
+    );
+
+    res.send('URL fetched successfully');
+  });
 };
